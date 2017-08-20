0862e2c95470b9f5c1974123767d466e833b21aa
Matias Dieguez <matiasdieguez@hotmail.com>
2017-08-20 02:12:31 -0300

    Agregado de proyecto .NET Core

---
+++ src/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ArgenSearch;
+
+namespace ArgenSearchCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(var i=31000000; i < 31000010; i++)
+            {
+                var result = ArgenSearchClient.Search(i).Result;
+                if(result.Success)
+                {
+                    var persona = ArgenSearchClient.Detail(result.Data[0]).Result;
+                    Console.WriteLine(result.Data[0] + " - " + persona.Data.Nombre);
+                }
+
+            }
+        }
+    }
+}
